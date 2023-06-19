5b4eea6955288e2141671176d54111d9d10c8d74
Asim Rasheed <asimrasheed442@gmail.com>
2023-06-19 17:58:46 +0500

    update

--- src/screens/dashboard/Categories/index.lazy.jsx
+++ src/screens/dashboard/Categories/index.lazy.jsx
@@ -1,32 +1,35 @@
 import {
   TableEntryDeleteButton,
-  TableEntryDescription,
   TableEntryEditButton,
   TableEntryImage,
   TableEntryText,
 } from "components";
+import { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
-import { Search } from "react-feather";
+import axios from "../../../utils/axios";
 import { useLocation } from "react-router-dom";
 
 export default function Categories() {
   const location = useLocation();
+  const [category, setCategory] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getData = () => {
+    setLoading(true);
+    axios.get("categories").then((res) => {
+      setCategory(res.data);
+      setLoading(false);
+    });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="container__main__content__listing">
       <div className="container__main__content__listing__header">
-        <div className="container__main__content__listing__header__left">
-          <form className="container__main__content__listing__header__left__search">
-            <input
-              type="text"
-              placeholder="Search"
-              className="container__main__content__listing__header__left__search__field"
-            />
-            <button className="container__main__content__listing__header__left__search__button">
-              <Search size={20} color="currentColor" />
-            </button>
-          </form>
-        </div>
+        <div className="container__main__content__listing__header__left"></div>
         <div className="container__main__content__listing__header__right">
           <Link
             to={location.pathname.toLowerCase() + "/add"}
@@ -44,62 +47,39 @@ export default function Categories() {
           <div className="container__main__content__listing__table__header__entry">
             Name
           </div>
-          <div className="container__main__content__listing__table__header__entry">
-            Description
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            Parent Category
-          </div>
+
           <div className="container__main__content__listing__table__header__entry">
             Image
           </div>
         </div>
         <div className="container__main__content__listing__table__content">
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
+          {category.map((item) => (
+            <TableEntry item={item} key={item._id} />
+          ))}
         </div>
       </div>
     </div>
   );
 }
 
-function TableEntry() {
+function TableEntry({ item }) {
   return (
     <div className="container__main__content__listing__table__content__list">
       <div className="container__main__content__listing__table__content__list__entry">
-        <TableEntryEditButton />
-        <TableEntryDeleteButton />
+        <TableEntryEditButton
+          to={`/dashboard/categories/edit/${item?._id}`}
+          state={{ ...item }}
+        />
+        <TableEntryDeleteButton
+          onClick={() => {
+            axios.delete(`categories/${item?._id}`).then((res) => {
+              console.log(res);
+            });
+          }}
+        />
       </div>
-      <TableEntryText>john devved</TableEntryText>
-      <TableEntryDescription>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor,
-        nisl eget ultricies lacinia, nisl nisl aliquam nisl, eu aliquam nisl
-        nisl sit amet nisl.
-      </TableEntryDescription>
-      <TableEntryText>Choice By Armina</TableEntryText>
-      <TableEntryImage />
+      <TableEntryText>{item?.name}</TableEntryText>
+      <TableEntryImage src={item?.img} />
     </div>
   );
 }
